35c66cc21b0a7f909e51326376b3794d422ea396
danher2 <daniel.hernandez8020@gmail.com>
2021-08-12 15:11:10 -0500

    front

---
+++ public/precache-manifest.6059cd9f6621b6406c689399d368f54c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75123f97a10afca717ac03e67243b29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c6d70866a929d0021a6",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "854cd105a5b5f4a12a95",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "3c6d70866a929d0021a6",
+    "url": "/static/js/2.5bd3e628.chunk.js"
+  },
+  {
+    "revision": "1f256c23eb2b723571f1cb4fe66dd398",
+    "url": "/static/js/2.5bd3e628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854cd105a5b5f4a12a95",
+    "url": "/static/js/main.1b485ee9.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
